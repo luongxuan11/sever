7f8c597e1fb54c4d9c01832c85196ef4139427be
Lương <Xluong2322001@gmail.com>
2023-08-26 09:46:55 +0700

    fix file sever

--- sever.js
+++ sever.js
@@ -1,10 +1,42 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import "../server/src/config/connectDB";
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import "../server/src/config/connectDB";
+// dotenv.config();
+
+// import initRouter from "./src/routes";
+// const app = express();
+
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL,
+//     optionsSuccessStatus: 200,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
+
+// app.use(express.json({limit: '10mb'}));
+// app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
+// initRouter(app);
+
+// const port = process.env.PORT || 3001;
+
+// const listener = app.listen(port, () => {
+//   console.log(`sever's running on the port ${listener.address().port}`);
+// });
+
+
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const path = require("path"); // Nếu cần sử dụng path
+
 dotenv.config();
 
-import initRouter from "./src/routes";
+require("../server/src/config/connectDB");
+
+const initRouter = require("./src/routes");
 const app = express();
 
 app.use(
@@ -15,13 +47,13 @@ app.use(
   })
 );
 
-app.use(express.json({limit: '10mb'}));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 initRouter(app);
 
 const port = process.env.PORT || 3001;
 
 const listener = app.listen(port, () => {
-  console.log(`sever's running on the port ${listener.address().port}`);
+  console.log(`Server's running on the port ${listener.address().port}`);
 });
